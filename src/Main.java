da74bf4dc3940db380cd38d237f735aecbb7490a
= <=>
2024-03-07 11:43:26 +0500

    learning Java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+
+    public static  int hello(){
+        return  100;
+    };
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        int marks = hello();
+        System.out.println(marks);
+    }
+}
---
+++ src/Subhan.java
@@ -0,0 +1,5 @@
+class Subhan{
+    public  static  void main(String[] args){
+        System.out.println("Learning Java");
+    }
+}
