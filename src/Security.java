9222133ac31e248456befd2361bfc36b0727f4b2
= <=>
2024-03-08 15:16:40 +0500

    add Security jaa fr fun

---
+++ src/Security.java
@@ -0,0 +1,17 @@
+public class Security {
+
+    public static void out(Object output){
+        System.out.println(output);
+    }
+
+    public static void main(String[] args) {
+
+        
+
+        out("THE SOFTWARE IS PROTECTED BY SECURITY-XIII SYSTEM BY JAVA-SEC-9 IN ADMINISTRATION OF COMPANY (MICROSOFT) - THESE PROGRAMS WILL BE EXEC BY COMPANY - LEGAL- [NONE]");
+
+
+
+        
+    }
+}
